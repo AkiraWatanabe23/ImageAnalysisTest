f87212bc557ee98156a48c2d9f829f6fad87b6be
Akira Watanabe <a0323w@gmail.com>
2023-06-10 15:50:45 +0900

    描画テスト

--- opencv_test.py
+++ opencv_test.py
@@ -1,3 +1,12 @@
+'''画像読み込みテスト'''
 import cv2
 
+#画像データのロード
 img = cv2.imread('onnpu.jpeg')
+
+#描画
+cv2.imshow('OpenCV', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('save.jpeg', img)
